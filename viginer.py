69552b034a2663a395c57afeb10bc026ea23f04b
ad1utant <mikolaj@marasek.eu>
2023-12-21 08:47:05 +0100

    update

---
+++ viginer.py
@@ -0,0 +1,11 @@
+import string
+def CreateTable():
+    alphabet,table = string.ascii_uppercase * 2, []
+    print(alphabet)
+    for i in range(len(alphabet)):
+        for j in range(i, i + len(alphabet)):
+            # table[i][j] =
+            pass
+
+
+CreateTable()

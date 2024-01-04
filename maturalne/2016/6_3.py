eff6a603a99a8c8d4332299020017f42a22b2ab0
ad1utant <mikolaj@marasek.eu>
2024-01-04 08:28:49 +0100

    update

---
+++ maturalne/2016/6_3.py
@@ -0,0 +1,14 @@
+import string
+
+
+def test_keys(word1, word2):
+    alphabet = string.ascii_uppercase + string.ascii_uppercase
+    i = alphabet.index(word1[0])
+    j = alphabet.index(word2[0])
+    k = j - i
+    for c1, c2 in zip(word1[1:], word2[1:]):
+        i = alphabet.index(c1)
+        j = alphabet.index(c2, i)
+        if j - k != 0:
+            return False
+        return True
